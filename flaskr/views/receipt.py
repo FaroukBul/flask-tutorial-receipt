45ef01d6ca9bcde7333dc8664f4226eb8cffd16c
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-05-10 19:48:24 -0500

    cleaned up html files and added link to reciepts

--- flaskr/views/receipt.py
+++ flaskr/views/receipt.py
@@ -3,11 +3,60 @@ from flask import (
     redirect, url_for
 )
 
+from . import get_form
+from flaskr.models.receipt import Receipt
+from flaskr.models.customer import Customer
 bp = Blueprint('receipt', __name__, url_prefix='/receipt')
 
 
-@bp.route('/receipt')
-def receipt():
+receipt_heads = [
+    'name',
+    'address',
+    'email'
+]
+
+@bp.route('/receipts')
+def receipts():
+    receipts = Receipt.get_all()
+    
     return render_template(
-        'receipt/receipt.html',
+        'receipt/receipts.html',
+        receipts=receipts,
+        heads=receipt_heads
     )
+
+
+@bp.route('/add', methods=('GET', 'POST'))
+def add():
+    form = get_form(receipt_heads)
+    if request.method == 'POST':
+        customer = Customer.search(form['name'])
+        receipt = Receipt(
+            customer_id=customer.id
+        )
+        receipt.add()
+    
+    return render_template(
+        'receipt/add.html',
+        heads=receipt_heads,
+        customer=None
+    )
+
+@bp.route('/update/<int:receipt_id>', methods=('GET', 'POST'))
+def update(receipt_id):
+    receipt = Receipt.get(receipt_id)
+    
+    if request.method == 'POST':
+        error = receipt.request.update()
+        if not error:
+            return redirect(
+                url_for('receipt.receipts')
+            )
+
+    return render_template(
+        'receipt/update.html',
+        customer=receipt.author,
+        heads=receipt_heads
+    )
+
+
