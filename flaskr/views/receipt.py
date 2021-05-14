72867d2400bdcf6a80931e6bffe990d2c4cb250f
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-05-13 21:33:14 -0500

    Display products as you add them

--- flaskr/views/receipt.py
+++ flaskr/views/receipt.py
@@ -20,7 +20,7 @@ receipt_customer_heads = [
 product_heads = [
     'name',
     'description',
-    'address'
+    'price'
 ]
 
 product_input = ['name']
@@ -62,27 +62,46 @@ def add():
 
 @bp.route('/update/<int:receipt_id>', methods=('GET', 'POST'))
 def update(receipt_id):
+    error = "Product already selected"
     product = None
     receipt = Receipt.get(receipt_id)
-    form = get_form(product_input)
+    sold_products = SoldProduct.search_receipt(receipt_id)
     if request.method == 'POST':
-        
+        form = get_form(product_input)
         product = Product.search(form['name'])
+        for sold in sold_products:
+            if sold.product.id == product.id:
+                flash(error)
+                return redirect(
+                    url_for('receipt.update', receipt_id=receipt.id)
+                )
         sold_product = SoldProduct(product_id=product.id, receipt_id=receipt.id)
         sold_product.add()
-        receipt.receipt = sold_product
-        receipt.update()
-        print(receipt.receipt)
-
-        
-        
+        sold_products = SoldProduct.search_receipt(receipt_id)
+        print(sold_products)
+    
     return render_template(
         'receipt/update.html',
         product=product,
         customer=None,
         receipt=receipt,
         heads=receipt_customer_heads,
-        product_heads=product_heads
+        product_heads=product_heads,
+        sold_products=sold_products
     )
 
+
+@bp.route('/delete/<int:receipt_id>/<int:product_id>', methods=("GET", "POST"))
+def delete_product(receipt_id ,product_id):
+    
+    SoldProduct.delete_product(receipt_id, product_id)
+
+    return redirect(url_for('receipt.update', receipt_id=receipt_id))
+
+@bp.route('/delete/<int:receipt_id>', methods=("GET", "POST"))
+def delete_receipt(receipt_id):
+    receipt = Receipt.get(receipt_id)
+    receipt.delete()
+
+    return redirect(url_for('receipt.receipts', receipt_id=receipt_id))
 
